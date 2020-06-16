2891e7878da854790b4862466cd00afdddbcc561
karlos6 <Carlos.Keller.M@gmail.com>
2020-06-16 18:28:01 -0500

    Union View y control (metodo separadorLenguaje)

---
+++ Control/controller.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+class controller:
+    
+    def separadoLenguaje(self, lenguaje):
+        separador = lenguaje.split("-")
+        print(separador)
+
--- View/windows.py
+++ View/windows.py
@@ -1,15 +1,16 @@
 from tkinter import *
+from Control.controller import controller
 
-class windows:
-    
-    
+class windows:     
 # Constructor de la ventana   
     def __init__(self):
-        self.ventana()
+        self.ventana()        
+        
         
 # Metodo que crea una ventana e inserta un panel de trabajo
     def ventana(self):
         self.view = Tk()
+        self.control = controller() 
         self.view.title("AUTOMATAS Y LENGUAJES FORMALES")
         self.view.geometry('1000x600+180+60')
         self.panel = Frame(self.view,width=1000, height=600).pack()
@@ -17,9 +18,20 @@ class windows:
         self.view.mainloop()
 
 # Inserta al panel un label y una variable que resive un string
-    def labelAndInput(self):
+    def labelAndInput(self):               
         self.variable_1 = StringVar()
-        self.label_1 = Label(self.panel,text="Instruccion: ").place(x=405, y=44)
-        self.input_1 = Entry(self.panel,textvariable = self.variable_1, width=10).place(x=475, y=44)
+        label_1 = Label(self.panel,text="Instruccion: ").place(x=405, y=44)
+        input_1 = Entry(self.panel,textvariable = self.variable_1, width=10).place(x=475, y=44)
+        print(self.variable_1)
+        boton1 = Button(self.panel,
+                             text="ACEPTAR",
+                             width=8,height=2,
+                             background="SkyBlue2",
+                             command= lambda: self.control.separadoLenguaje(self.variable_1.get())
+                             ).place(x=425,y=70)
+
+       
         
+        
+    
 
